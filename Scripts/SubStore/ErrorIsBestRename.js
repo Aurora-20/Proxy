c0215b29d2dba913ce27fb85c395d6a3614ad1b9
Aurora <sllxy20@hotmail.com>
2023-12-17 16:39:54 +0800

    增加 EiB 重命名脚本

---
+++ Scripts/SubStore/ErrorIsBestRename.js
@@ -0,0 +1,25 @@
+function operator(proxies) {
+  const other = /-\(.+\)$|---|消耗/g;
+  const hk = /香港|HK/;
+  const sg = /新加坡|SG/;
+  const us = /美国|US/;
+  const jp = /日本|JP/;
+  const tw = /台湾|TW/;
+  proxies.forEach((proxy) => {
+    proxy.name = proxy.name.replace(other, "");
+    proxy.name = proxy.name.replace(/(\d\.\d+)/, "$1x");
+    if (hk.test(proxy.name)) {
+      proxy.name = proxy.name.replace(hk, "Hong Kong ");
+    } else if (sg.test(proxy.name)) {
+      proxy.name = proxy.name.replace(sg, "Singapore ");
+    } else if (us.test(proxy.name)) {
+      proxy.name = proxy.name.replace(us, "United States ");
+    } else if (jp.test(proxy.name)) {
+      proxy.name = proxy.name.replace(jp, "Japan ");
+    } else if (tw.test(proxy.name)) {
+      proxy.name = proxy.name.replace(tw, "Taiwan ");
+    }
+  });
+
+  return proxies;
+}
